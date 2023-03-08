09d780629a9184b879be2e70cc0eb5b16d833ea3
billyosafat <bill.yosafat@sangkuriang.co.id>
2023-03-08 18:47:22 +0700

    update(modul): tambah menu panduan

---
+++ Controllers/PanduanController.cs
@@ -0,0 +1,14 @@
+﻿using Pnbp.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Pnbp.Controllers
+{
+    public class PanduanController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
